7ba70a7088543533726d329d4bf42c6225b23bd7
Lucky Wirasakti <lucky.adhikrisna@erasysconsulting.com>
2021-06-14 15:21:25 +0700

    update release

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
-mkdir -p core/static
-cp core/.env.example core/.env
-python3 manage.py collectstatic --no-input
+mkdir -p core/issue/migrations &
+mkdir -p static &
+cp core/.env.example core/.env &
+python3 manage.py collectstatic --no-input 
 python3 manage.py makemigrations --no-input
 python3 manage.py migrate --no-input
